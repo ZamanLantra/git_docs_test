acc73bd06c6c8d045ba7c5e4410e3ce714d62a80
ZamanLantra <92105524+ZamanLantra@users.noreply.github.com>
2024-05-23 11:35:54 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
+project = 'OP-PIC'
+# copyright = 'workshop participant'
+# author = 'workshop participant'
 release = '0.1'
 
 
