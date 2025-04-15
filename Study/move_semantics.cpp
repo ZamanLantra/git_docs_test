ed57484f3c26cb2925b967de2557377521362ed5
ZamanLantra <zaman.lantra@warwick.ac.uk>
2025-04-15 14:15:09 +0100

    move

--- Study/move_semantics.cpp
+++ Study/move_semantics.cpp
@@ -6,6 +6,17 @@
 
 using namespace std;
 
+void* operator new(size_t bytes) {
+    cout << "Allocating " << bytes << " bytes\n";
+    void* ptr = std::malloc(bytes);
+    if (!ptr) throw std::bad_alloc();
+    return ptr;
+}
+void operator delete(void* ptr) noexcept {
+    cout << "Deleting\n";
+    std::free(ptr);
+}
+
 class String {
 public:
     String() = delete;
@@ -91,43 +102,43 @@ int main() {
     s6_const.print("s6_const->");
     s7_const.print("s7_const->");
     
-    cout << "\nStart Copy consructor s2\n";
+    cout << "\nStart Copy consructor s2 -------\n";
     String s2 = s1;                         // Uses the Copy consructor
     s2.print("s2->");
     
-    cout << "\nStart Copy Assignemnt s3\n";
+    cout << "\nStart Copy Assignemnt s3 -------\n";
     String s3("Hello");                     // Uses the constructor
     s3 = s1;                                // Uses the Copy assignment
     s3.print("s3->");
     
-    cout << "\nStart Move consructor s4\n";
+    cout << "\nStart Move consructor s4 -------\n";
     String s4 = std::move(s1);              // Uses the Move Constructor - explicit
     s4.print("s4->");
     s1.print("s1->");
     
-    cout << "\nStart Move Assignemnt s5\n";
+    cout << "\nStart Move Assignemnt s5 -------\n";
     String s5("Hello");                     // Uses the constructor
     s5 = std::move(s4);                     // Uses the Move Assignment
     s4.print("s4->");
     s5.print("s5->");
     
-    cout << "\nConcatenate s8\n";
+    cout << "\nConcatenate s8 -------\n";
     String s8 = s6 + s7;                    // Uses the Concat operator and Move Constructor - implicit
     s8.print("s8->");
     
-    cout << "\nConcatenate s9\n";
+    cout << "\nConcatenate s9 -------\n";
     String s9(s6 + s7);                     // Uses the Concat operator and Move Constructor - implicit
     s9.print("s9->");
     
-    cout << "\nConcatenate s10\n";
+    cout << "\nConcatenate s10 -------\n";
     String s10 = s6_const + s7_const;       // Uses the Concat operator and Move Constructor - implicit
     s10.print("s10->");
     
-    cout << "\nConcatenate s11\n";
+    cout << "\nConcatenate s11 -------\n";
     String s11(s6_const + s7_const);        // Uses the Concat operator and Move Constructor - implicit
     s11.print("s11->");
     
-    cout << "\nConcatenate s12\n";
+    cout << "\nConcatenate s12 -------\n";
     String s12(String("Hi") + String("There")); // Uses the Concat operator and Move Constructor - implicit
     s12.print("s12->");                         // But in this example, we get Hi and There strings constructed first
                                                 // As we have Concat, anyway we need to create a bigger string to have both, 
