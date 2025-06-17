b63e62947b25cad1f428900471c6535ba6d762a6
Zaman Lantra <92105524+ZamanLantra@users.noreply.github.com>
2025-06-17 21:19:06 +0100

    Create TestQueue.cpp

---
+++ HFT/TestQueue.cpp
@@ -0,0 +1,35 @@
+#include "LockLessQueue.hpp"
+
+template <typename Q>
+void testQueue(const std::string& queueType) {
+    std::cout << "Testing with " << queueType << "...\n";
+    
+    Queue<Q> queue(128);
+  
+    double* msg1 = new double(5.0);
+    double* msg2 = new double(6.0);  
+    queue.enqueue(msg1);
+    queue.enqueue(msg2);
+  
+    double* received1 = queue.dequeue();
+    double* received2 = queue.dequeue();
+    
+    if (received1) {
+        std::cout << "Received: " << *received1 << std::endl;      
+    } else {
+        std::cout << "Queue received1 is empty." << std::endl;
+    }
+    if (received2) {
+        std::cout << "Received: " << *received2 << std::endl;
+    } else {
+        std::cout << "Queue received2 is empty." << std::endl;
+    }
+    delete received1, received2;
+}
+
+int main() { 
+    testQueue<LockedQueue<double*>>("LockedQueue");
+    testQueue<CustomSPMCLockFreeQueue<double*>>("CustomSPMCLockFreeQueue");
+    testQueue<BoostLockFreeQueue<double*>>("BoostLockFreeQueue");
+    testQueue<MoodycamelLockFreeQueue<double*>>("MoodycamelLockFreeQueue");
+}
