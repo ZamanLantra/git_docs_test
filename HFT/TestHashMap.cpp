48db39aa80ed03990bb207bb32af43ccafa1f612
Zaman Lantra <92105524+ZamanLantra@users.noreply.github.com>
2025-06-18 10:41:00 +0100

    Create TestHashMap.cpp

---
+++ HFT/TestHashMap.cpp
@@ -0,0 +1,47 @@
+// g++ -std=c++20 TestHashMap.cpp -o TestHashMap -O3 -DHASH_BUCKETS=32
+
+#include "HashMap.hpp"
+
+/**************************************************************************/
+template <typename HM>
+void testHashMap(const std::string& hmType) {
+    std::cout << "Testing with " << hmType << "...\n";
+
+    HashMap<HM> hm;
+
+    hm.insert(1, "one");
+    hm.insert(2, "two");
+    hm.insert(3, "three");
+
+    if (hm.contains(2)) {
+        std::cout << "Key 2 exists in the hash map." << std::endl;
+    } else {
+        std::cout << "Key 2 does not exist in the hash map." << std::endl;
+    }
+
+    hm.remove(2);
+    
+    if (!hm.contains(2)) {
+        std::cout << "Key 2 has been removed from the hash map." << std::endl;
+    }
+
+    std::string* value3 = hm.find(3);
+    if (value3) {
+        std::cout << "Found key 3 with value: " << *value3 << std::endl;
+    } else {
+        std::cout << "Key 3 not found in the hash map." << std::endl;
+    }
+    std::string* value4 = hm.find(4);
+    if (value4) {
+        std::cout << "Found key 4 with value: " << *value4 << std::endl;
+    } else {
+        std::cout << "Key 4 not found in the hash map." << std::endl;
+    }
+}
+
+int main() {
+    
+    testHashMap<ChainingHashMap<int, std::string>>("ChainingHashMap<int, std::string>");
+    testHashMap<FixedSizedChainingHashMap<int, std::string>>("FixedSizedChainingHashMap<int, std::string>");
+    testHashMap<OpenAddressingHashMap<int, std::string>>("OpenAddressingHashMap<int, std::string>");
+}
